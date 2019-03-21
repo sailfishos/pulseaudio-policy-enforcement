b91a06fbddaf4eba2b2005aa12b291e936084b0a
Juho Hämäläinen <juho.hamalainen@jolla.com>
2019-03-21 14:54:05 +0200

    policy: Add send card state function.

--- src/policy.h
+++ src/policy.h
@@ -10,5 +10,7 @@
 void pa_policy_send_device_state(struct userdata *u, const char *state,
                                  const struct pa_classify_result *list);
 void pa_policy_send_device_state_full(struct userdata *u);
+void pa_policy_send_card_state(struct userdata *u, const struct pa_classify_result *list,
+                               const char *profile);
 
 #endif
