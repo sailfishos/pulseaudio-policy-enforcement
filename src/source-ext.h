c451dbdecfc4eae66d6a6681f5fd5fd90a660b14
Juho Hämäläinen <juho.hamalainen@jolla.com>
2019-10-11 11:47:07 +0300

    source-ext: Handle null source.

--- src/source-ext.h
+++ src/source-ext.h
@@ -5,6 +5,11 @@
 
 struct pa_source;
 
+struct pa_null_source {
+    char            *name;
+    pa_source       *source;
+};
+
 struct pa_source_evsubscr {
     pa_hook_slot    *put;
     pa_hook_slot    *unlink;
@@ -16,6 +21,8 @@ void  pa_source_ext_discover(struct userdata *);
 const char *pa_source_ext_get_name(struct pa_source *);
 int   pa_source_ext_set_mute(struct userdata *, const char *, int);
 int   pa_source_ext_set_ports(struct userdata *, const char *);
+struct pa_null_source *pa_source_ext_init_null_source(const char *name);
+void pa_source_ext_null_source_free(struct pa_null_source *);
 
 #endif /* foosourceextfoo */
 
